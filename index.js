70fa54292cfe93196030dd302d8383c26539c84f
Dang Tinh <>
2022-05-12 17:32:04 +0700

    3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const
     app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 3003, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
 
 app.get('/webhook', (req, res) => {
 
